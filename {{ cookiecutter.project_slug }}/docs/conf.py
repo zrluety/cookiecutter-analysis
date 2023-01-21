31db52318e0731bcc6967a2a88cef2e9dff54a1b
Zachary Luety <zrluety216@gmail.com>
2023-01-21 00:18:13 -0600

    Remove messaging after conda env creation

--- {{ cookiecutter.project_slug }}/docs/conf.py
+++ {{ cookiecutter.project_slug }}/docs/conf.py
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 # html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = '{{ cookiecutter.repo_name }}doc'
+htmlhelp_basename = '{{ cookiecutter.project_slug }}doc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
     ('index',
-     '{{ cookiecutter.repo_name }}.tex',
+     '{{ cookiecutter.project_slug }}.tex',
      u'{{ cookiecutter.project_name }} Documentation',
      u"{{ cookiecutter.author_name }}", 'manual'),
 ]
@@ -215,7 +215,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', '{{ cookiecutter.repo_name }}', u'{{ cookiecutter.project_name }} Documentation',
+    ('index', '{{ cookiecutter.project_slug }}', u'{{ cookiecutter.project_name }} Documentation',
      [u"{{ cookiecutter.author_name }}"], 1)
 ]
 
@@ -229,9 +229,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', '{{ cookiecutter.repo_name }}', u'{{ cookiecutter.project_name }} Documentation',
+    ('index', '{{ cookiecutter.project_slug }}', u'{{ cookiecutter.project_name }} Documentation',
      u"{{ cookiecutter.author_name }}", '{{ cookiecutter.project_name }}',
-     '{{ cookiecutter.description }}', 'Miscellaneous'),
+     , 'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
